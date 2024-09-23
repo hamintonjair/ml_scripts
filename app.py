acee1597e700f5ea9cb154ae749fe9d9d50cdf1e
hamintonjair <85085152+hamintonjair@users.noreply.github.com>
2024-09-23 05:56:14 -0500

    app.py

--- app.py
+++ app.py
@@ -474,5 +474,6 @@ def predicciones():
     return jsonify({"mensaje": "Predicción realizada con éxito.", "datos": data})
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", default=5000))  # Usa el puerto de Render o 5000 por defecto
-    app.run(debug=True,host='0.0.0.0', port=port)
+    # port = int(os.environ.get("PORT", default=5000))  # Usa el puerto de Render o 5000 por defecto
+    port = os.getenv("PORT", default=5000)) 
+    app.run(debug=True,host="0.0.0.0", port=port)
